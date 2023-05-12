dd4f30952085f17152a8478d7dfc095106553363
Renee Paczkowski <rpaczkowski@gmail.com>
2023-05-12 10:34:24 -0400

    Played around with default hard coded text

--- src/App.js
+++ src/App.js
@@ -11,16 +11,9 @@ render () {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Ren!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Change name</button>
       </header>
     </div>
   );
