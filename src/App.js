f41db219a0bd97e85fdb65caaa86eba37c9bc1a6
Renee Paczkowski <rpaczkowski@gmail.com>
2023-05-22 11:20:51 -0400

    Changed from class to function component

--- src/App.js
+++ src/App.js
@@ -3,58 +3,72 @@ import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
-class App extends Component {
-  // always runs first, initiallizes the state
-  constructor() {
-    super();
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+// class App extends Component {
+//   // always runs first, initiallizes the state
+//   constructor() {
+//     super();
 
-  // runs after the render
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//   }
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
+//   // runs after the render
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { monsters: users };
+//         })
+//       );
+//   }
 
-    this.setState(() => {
-      return { searchField };
-    });
-  };
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
 
-  // runs second, dictates what the UI will be
-  render() {
-    console.log("render from AppJS");
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   };
 
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
+//   // runs second, dictates what the UI will be
+//   render() {
+//     console.log("render from AppJS");
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          className="search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+//         <SearchBox
+//           className="search-box"
+//           onChangeHandler={onSearchChange}
+//           placeholder="search monsters"
+//         />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
